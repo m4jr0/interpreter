b166b63663901fc202d80d9fbcf49fb71b5a28fd
m4jr0 <cosmicdrift0@pm.me>
2026-09-20 10:30:11 +0200

    Add closures

--- clox/src/object.c
+++ clox/src/object.c
@@ -18,9 +18,24 @@ static Obj *allocateObject(size_t size, ObjType type) {
   return object;
 }
 
+ObjClosure *newClosure(ObjFunction *function) {
+  ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue *, function->upvalueCount);
+
+  for (int i = 0; i < function->upvalueCount; i++) {
+    upvalues[i] = NULL;
+  }
+
+  ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+  closure->function = function;
+  closure->upvalues = upvalues;
+  closure->upvalueCount = function->upvalueCount;
+  return closure;
+}
+
 ObjFunction *newFunction(void) {
   ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
   function->arity = 0;
+  function->upvalueCount = 0;
   function->name = NULL;
   initChunk(&function->chunk);
   return function;
@@ -30,6 +45,14 @@ ObjNative *newNative(NativeFn function) {
   ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
   native->function = function;
   return native;
+}
+
+ObjUpvalue *newUpvalue(Value *slot) {
+  ObjUpvalue *upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
+  upvalue->location = slot;
+  upvalue->closed = NIL_VAL;
+  upvalue->next = NULL;
+  return upvalue;
 }
 
 static uint32_t hashString(const char *key, int length) {
@@ -49,12 +72,16 @@ static ObjString *allocateString(char *chars, int length, uint32_t hash) {
   string->chars = chars;
   string->hash = hash;
 
+  push(OBJ_VAL(string));
   tableSet(&vm.strings, string, NIL_VAL);
+  pop();
+
   return string;
 }
 
 ObjString *takeString(char *chars, int length) {
   uint32_t hash = hashString(chars, length);
+
   ObjString *interned = tableFindString(&vm.strings, chars, length, hash);
 
   if (interned != NULL) {
@@ -67,11 +94,11 @@ ObjString *takeString(char *chars, int length) {
 
 ObjString *copyString(const char *chars, int length) {
   uint32_t hash = hashString(chars, length);
+
   ObjString *interned = tableFindString(&vm.strings, chars, length, hash);
 
-  if (interned != NULL) {
+  if (interned != NULL)
     return interned;
-  }
 
   char *heapChars = ALLOCATE(char, length + 1);
   memcpy(heapChars, chars, length);
@@ -91,6 +118,10 @@ static void printFunction(ObjFunction *function) {
 
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
+  case OBJ_CLOSURE:
+    printFunction(AS_CLOSURE(value)->function);
+    break;
+
   case OBJ_FUNCTION:
     printFunction(AS_FUNCTION(value));
     break;
@@ -101,6 +132,10 @@ void printObject(Value value) {
 
   case OBJ_STRING:
     printf("%s", AS_CSTRING(value));
+    break;
+
+  case OBJ_UPVALUE:
+    printf("upvalue");
     break;
   }
 }
